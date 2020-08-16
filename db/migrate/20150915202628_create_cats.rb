de47b3f4162f064e45e5fcf8aa58a857e4ddeb42
Michael Fahim <michael_fahim@yahoo.com>
2020-08-16 00:45:23 +0000

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -6,8 +6,15 @@ class CreateCats < ActiveRecord::Migration
       t.string :breed
     end
   end
-  
+
   def down
     drop_table :cats
   end
+  def change
+  create_table :cats do |t|
+    t.string :name
+    t.integer :age
+    t.string :breed
+  end
+end
 end
